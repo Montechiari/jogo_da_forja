6ccfc0d6ff7a7f5634ad2cf608715e6a5956cf7f
Gabriel Montechiari <g.monte.silva@gmail.com>
2020-01-21 16:31:16 -0300

    Update combate.py
    Processamento de mensagens de qualquer maneira, pra refatorar depois.

--- combate.py
+++ combate.py
@@ -1,8 +1,10 @@
 import numpy as np
+import operator
+from functools import reduce
 
-SAUDE_PRONTIDAO_MAX = 20
+SAUDE_PRONTIDAO_MAX = 25
 SAUDE_PRONTIDAO_MIN = 8
-DANO_ARMA_MAX = 10
+DANO_ARMA_MAX = 8
 DANO_ARMA_MIN = 3
 
 # Caractere da esquerda eh acao do combatente com iniciativa; direita, do outro
@@ -25,9 +27,9 @@ ACOES_CODIFICADAS = {"0": ['ofensiva', 0, '', 0],
                      "8": ['', 0, '', 1],
                      "9": ['', 0, '', 0]}
 
-NOME_DE_ACAO = ['movimento ofensivo.', 'movimento defensivo.',
-                'ataque corte.', 'ataque estocada.',
-                'defesa corte.', 'defesa estocada.']
+NOME_DE_ACAO = ['movimento ofensivo', 'movimento defensivo',
+                'ataque corte', 'ataque estocada',
+                'defesa corte', 'defesa estocada']
 
 
 class Arma():
@@ -128,16 +130,18 @@ class JogadorAleatorio(Combatente):
 
     def decide_acao(self):
         self.acao = np.random.randint(1, 7)
+        return self.acao
 
 
 class RegistroDeTurno():
     def __init__(self):
         pass
 
-    def registro_permanente(self, combatentes):
-        self.registro = {}
+    def coleta_dados_iniciais(self, combatentes):
+        self.dados_iniciais = {}
         for combatente in combatentes:
-            self.registro[combatente.nome] = []
+            self.dados_iniciais[combatente.nome] = [combatente.saude,
+                                                    combatente.prontidao]
 
 
 class Vantagem():
@@ -147,11 +151,12 @@ class Vantagem():
 
 
 class Partida():
-    def __init__(self, imprimir=True):
+    def __init__(self):
 
-        self.imprimir = imprimir
         self.vantagem = Vantagem()
+        self.registro = RegistroDeTurno()
         self.combatentes = self.cria_combatentes()
+        self.registro.coleta_dados_iniciais(self.combatentes)
         self.turno_numero = 1
         while self.todos_vivos() and self.turno_numero < 20:
             self.novo_turno()
@@ -171,13 +176,27 @@ class Partida():
 
         for combatente in self.combatentes:
             combatente.decide_acao()
+        print(self.mensagem_sobre_acao())
 
         self.traduz_acoes(*self.combatentes)
 
-        for combatente in self.combatentes:
-            print(self.aplica_efeitos_de_acao(combatente))
+        # pontuacao = [' e ', "."]
+        dicionario_de_efeitos = {self.combatentes[0].nome: [],
+                                 self.combatentes[1].nome: []}
+        frases = [self.aplica_efeitos_de_acao(combatente)
+                  for combatente in self.combatentes]
+        conjunto = []
+        for frase in frases:
+            conjunto.extend(frase)
+        for frase in conjunto:
+            dicionario_de_efeitos[frase[0]].append(frase[1])
+        self.imprime_consequencias(dicionario_de_efeitos)
 
         while numero_de_acoes_extras > 0:
+            mensagem = ["Ação extra: –", self.combatentes[0].nome,
+                        "realizou",
+                        NOME_DE_ACAO[self.combatentes[0].decide_acao() - 1]]
+            print(" ".join(mensagem))
             self.combatentes[0].decide_acao()
             self.traduz_acoes(*self.combatentes, acao_extra=True)
             self.aplica_efeitos_de_acao(self.combatentes[0])
@@ -196,7 +215,7 @@ class Partida():
                 combatente.acao = ACOES_CODIFICADAS[indice_de_acao[i]]
 
     def aplica_efeitos_de_acao(self, combatente):
-        relatorio = [combatente]
+        relatorio = []
         self.resolve_prontidao(combatente)
         relatorio.append(self.resolve_dano(combatente))
         if combatente.acao[0] != '':
@@ -207,7 +226,9 @@ class Partida():
         self.vantagem = Vantagem(combatente, combatente.acao[0])
         combatente.oponente.tem_vantagem = False
         combatente.tem_vantagem = True
-        return "assume vantagem {tipo}".format(tipo=self.vantagem.tipo)
+        saida = [combatente.nome,
+                 "assume vantagem {tipo}".format(tipo=self.vantagem.tipo)]
+        return saida
 
     def resolve_dano(self, combatente):
         dano = 0
@@ -222,7 +243,10 @@ class Partida():
             dano *= 0.5
         if dano > 0:
             combatente.oponente.altera_saude(-dano)
-            return "recebe {dano} pontos de dano".format(dano=dano)
+            return [combatente.oponente.nome,
+                    "recebe {dano} pontos de dano".format(dano=dano)]
+        else:
+            return [combatente.oponente.nome, ""]
 
     def resolve_prontidao(self, combatente):
         prontidao_nova = combatente.acao[3]
@@ -259,6 +283,29 @@ class Partida():
                     ) if self.vantagem.quem is not None else "Ninguém tem \
 vantagem."
         print(superior, "\n" + iniciativa, vantagem)
+
+    def mensagem_sobre_acao(self):
+        pontuacao = [" e ", "."]
+        acoes_por_extenso = ["{nome} realizou {acao}{ponto}".format(
+            nome=combatente.nome,
+            acao=NOME_DE_ACAO[combatente.acao - 1],
+            ponto=pontuacao[i]
+                                                                    )
+                             for i, combatente in enumerate(self.combatentes)]
+        return "".join(acoes_por_extenso)
+
+    def imprime_consequencias(self, dicionario):
+        pontuacao = [" e ", "."]
+        for key in dicionario.keys():
+            if '' in dicionario[key]:
+                dicionario[key].remove('')
+            if dicionario[key] != []:
+                mensagem = []
+                for i, frase in enumerate(dicionario[key]):
+                    mensagem.append(frase + pontuacao[i - 1])
+                print("–", key, "".join(reversed(mensagem)))
+            else:
+                print("– Não houve consequência para {nome}.".format(nome=key))
 
 
 if __name__ == '__main__':
